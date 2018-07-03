3453a971c948147d48f61b55690f19926e960674
Deric Aaron <aaro0034@umn.edu>
2018-07-02 22:23:29 -0500

    test resume screen layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">GitHero</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <br/>
+        <div className="container">
+          <div className="nameOverlay">
+            <h1 className="headText">Deric Aaron</h1>
+          </div>
+          <img src="https://ghchart.rshah.org/DericAaron" alt="" className="headerImg"/>
+        </div>
+        
+        <br/>
+        <br/>
+        <div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+        
       </div>
     );
   }
