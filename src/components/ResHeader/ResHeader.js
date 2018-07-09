5b6277273dc70b90331818fc184c894b7a84fc44
Deric Aaron <aaro0034@umn.edu>
2018-07-08 21:21:41 -0500

    componantized projects, header, and card-Header

---
+++ src/components/ResHeader/ResHeader.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ResHeader extends Component {
+
+  render() {
+
+    return (
+        <div className="container">
+          <div className="nameOverlay">
+            <h1 className="headText">Deric Aaron</h1>
+          </div>
+          <img src="https://ghchart.rshah.org/DericAaron" alt="" className="headerImg"/>
+        </div>
+
+    );
+  }
+}
+
+export default ResHeader;
