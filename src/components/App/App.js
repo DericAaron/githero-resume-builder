363c39c8f602a979e84309f82133b9187444de5b
Deric Aaron <aaro0034@umn.edu>
2018-07-08 21:04:52 -0500

    card state functionality added. User can now switch between description and code pane

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,48 @@ import logo from '../../logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {view: 1}
+  }
+
+  stateShift = (newState) => {
+    this.setState({view: newState});
+  } //end state shift
+
   render() {
+
+    let content = null;
+    switch (this.state.view) {
+      case 1: content = (
+        <div>
+          <img src="https://img1.southernliving.timeinc.net/sites/default/files/styles/story_card_hero/public/image/2017/07/main/slow-cooker_pork_tacos_with_fresh_tomato_salsa_2487601_summe_0064.jpg?itok=Xdg39X7i" alt=""/>
+
+          <div className="description">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et obcaecati vel pariatur cupiditate quia ratione porro deleniti, unde sunt quae aspernatur temporibus voluptate laudantium dolorem expedita consequatur ea? Corporis, debitis?</p>
+          </div>
+        </div>
+        ); break;
+
+      case 2:
+        content = (
+          <div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa impedit repellendus repellat pariatur architecto dolor obcaecati nisi assumenda atque dolore! Fugit, iure! Repudiandae molestiae quas eligendi natus odio perferendis est?</p>
+          </div>
+        );
+        break;
+
+      default:
+        break;
+    }
     return (
       <div className="App">
       
         <header className="App-header">
           <h1 className="App-title">GitHero</h1>
         </header>
-
+      {/* Header with git commits */}
         <br/>
         <div className="container">
           <div className="nameOverlay">
@@ -18,6 +52,7 @@ class App extends Component {
           </div>
           <img src="https://ghchart.rshah.org/DericAaron" alt="" className="headerImg"/>
         </div>
+        {/* end header with git */}
         
         <br/>
         <br/>
@@ -27,19 +62,20 @@ class App extends Component {
           <div className="card-header">
             <h5>Project Name</h5>
             <br/>
+
             <div>
-              <button>Source</button>
+              <button onClick={() => this.stateShift(1)}>Layout</button>
+              <button onClick={() => this.stateShift(2)}>Source</button>
+              <a href="https://github.com/DericAaron/movie-collection-database" target="_blank">
               <button>Git</button>
+              </a>
             </div>
             
           </div>
-
-          <img src="https://img1.southernliving.timeinc.net/sites/default/files/styles/story_card_hero/public/image/2017/07/main/slow-cooker_pork_tacos_with_fresh_tomato_salsa_2487601_summe_0064.jpg?itok=Xdg39X7i" alt=""/>
-
-          <div className="description">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et obcaecati vel pariatur cupiditate quia ratione porro deleniti, unde sunt quae aspernatur temporibus voluptate laudantium dolorem expedita consequatur ea? Corporis, debitis?</p>
-          </div>
+          {content}
+         
         </div> 
+        {/* end card area */}
         
       </div>
     );
